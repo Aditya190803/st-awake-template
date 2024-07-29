1addbe52320c25d208b877a3408a18f241d44f83
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-07-29 18:05:43 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+# List of Streamlit App URLs
+
+STREAMLIT_APPS = [
+    "https://example1.streamlit.app/",
+    "https://example2.streamlit.app/",
+    "https://example3.streamlit.app/",
+    "https://example4.streamlit.app/",
+    "https://example5.streamlit.app/",
+    "https://example6.streamlit.app/",
+    "https://example7.streamlit.app/",
+    "https://example8.streamlit.app/",
+    "https://example9.streamlit.app/",
+    "https://example10.streamlit.app/"
+]
