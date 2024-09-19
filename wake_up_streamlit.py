85122e2f4abd6b62a82426a6bd28a2db05fb47e0
Aditya190803 <101453576+Aditya190803@users.noreply.github.com>
2024-09-19 09:13:36 +0530

    Update wake_up_streamlit.py

--- wake_up_streamlit.py
+++ wake_up_streamlit.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-from streamlit_apps import STREAMLIT_APPS
+from streamlit_app import STREAMLIT_APPS
 import datetime
 
 # Set up Selenium webdriver (assuming Chrome)
